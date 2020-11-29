4ee25261be3a886de58427b7fdb5c28cfe822b47
danel.llaurador@upr.edu <danel.llaurador@upr.edu>
2020-11-28 20:54:49 -0800

    Add project files.

---
+++ InventoryForm.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace FinalProjectVersion2
+{
+    partial class InventoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBanner = new System.Windows.Forms.Label();
+            this.dgvInventory = new System.Windows.Forms.DataGridView();
+            this.Product_ID_Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Product_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Stock_Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price_Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sports_Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblProductId = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblSportsCategory = new System.Windows.Forms.Label();
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtSportsCategory = new System.Windows.Forms.TextBox();
+            this.btnShowProducts = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEditProducts = new System.Windows.Forms.Button();
+            this.btnDeleteProducts = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBanner
+            // 
+            this.lblBanner.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBanner.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblBanner.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBanner.Location = new System.Drawing.Point(-1, 0);
+            this.lblBanner.Name = "lblBanner";
+            this.lblBanner.Size = new System.Drawing.Size(978, 114);
+            this.lblBanner.TabIndex = 0;
+            this.lblBanner.Text = "Sports Equipment Inventory";
+            this.lblBanner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgvInventory
+            // 
+            this.dgvInventory.AllowUserToAddRows = false;
+            this.dgvInventory.AllowUserToDeleteRows = false;
+            this.dgvInventory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventory.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Product_ID_Column,
+            this.Product_Name,
+            this.Stock_Column,
+            this.Price_Column,
+            this.Sports_Category});
+            this.dgvInventory.Location = new System.Drawing.Point(409, 149);
+            this.dgvInventory.Name = "dgvInventory";
+            this.dgvInventory.ReadOnly = true;
+            this.dgvInventory.Size = new System.Drawing.Size(546, 258);
+            this.dgvInventory.TabIndex = 1;
+            this.dgvInventory.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInventory_CellClick);
+            // 
+            // Product_ID_Column
+            // 
+            this.Product_ID_Column.HeaderText = "Product_ID";
+            this.Product_ID_Column.Name = "Product_ID_Column";
+            this.Product_ID_Column.ReadOnly = true;
+            // 
+            // Product_Name
+            // 
+            this.Product_Name.HeaderText = "Product_Name";
+            this.Product_Name.Name = "Product_Name";
+            this.Product_Name.ReadOnly = true;
+            // 
+            // Stock_Column
+            // 
+            this.Stock_Column.HeaderText = "Stock";
+            this.Stock_Column.Name = "Stock_Column";
+            this.Stock_Column.ReadOnly = true;
+            // 
+            // Price_Column
+            // 
+            this.Price_Column.HeaderText = "Price";
+            this.Price_Column.Name = "Price_Column";
+            this.Price_Column.ReadOnly = true;
+            // 
+            // Sports_Category
+            // 
+            this.Sports_Category.HeaderText = "Sports_Category";
+            this.Sports_Category.Name = "Sports_Category";
+            this.Sports_Category.ReadOnly = true;
+            // 
+            // lblProductId
+            // 
+            this.lblProductId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductId.Location = new System.Drawing.Point(22, 168);
+            this.lblProductId.Name = "lblProductId";
+            this.lblProductId.Size = new System.Drawing.Size(100, 23);
+            this.lblProductId.TabIndex = 2;
+            this.lblProductId.Text = "Product Id:";
+            this.lblProductId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.Location = new System.Drawing.Point(22, 211);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(120, 23);
+            this.lblProductName.TabIndex = 3;
+            this.lblProductName.Text = "Product Name:";
+            this.lblProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblStock
+            // 
+            this.lblStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStock.Location = new System.Drawing.Point(22, 250);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(178, 23);
+            this.lblStock.TabIndex = 4;
+            this.lblStock.Text = "Stock:";
+            this.lblStock.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(22, 293);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(100, 23);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Price:";
+            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSportsCategory
+            // 
+            this.lblSportsCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSportsCategory.Location = new System.Drawing.Point(22, 333);
+            this.lblSportsCategory.Name = "lblSportsCategory";
+            this.lblSportsCategory.Size = new System.Drawing.Size(128, 23);
+            this.lblSportsCategory.TabIndex = 6;
+            this.lblSportsCategory.Text = "Sports Category:";
+            this.lblSportsCategory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(249, 168);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(100, 20);
+            this.txtProductId.TabIndex = 7;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(249, 211);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 8;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(249, 250);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 9;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(249, 293);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 10;
+            // 
+            // txtSportsCategory
+            // 
+            this.txtSportsCategory.Location = new System.Drawing.Point(249, 333);
+            this.txtSportsCategory.Name = "txtSportsCategory";
+            this.txtSportsCategory.Size = new System.Drawing.Size(100, 20);
+            this.txtSportsCategory.TabIndex = 11;
+            // 
+            // btnShowProducts
+            // 
+            this.btnShowProducts.Location = new System.Drawing.Point(249, 91);
+            this.btnShowProducts.Name = "btnShowProducts";
+            this.btnShowProducts.Size = new System.Drawing.Size(75, 23);
+            this.btnShowProducts.TabIndex = 12;
+            this.btnShowProducts.Text = "Products";
+            this.btnShowProducts.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(26, 384);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEditProducts
+            // 
+            this.btnEditProducts.Location = new System.Drawing.Point(151, 384);
+            this.btnEditProducts.Name = "btnEditProducts";
+            this.btnEditProducts.Size = new System.Drawing.Size(75, 23);
+            this.btnEditProducts.TabIndex = 14;
+            this.btnEditProducts.Text = "Edit";
+            this.btnEditProducts.UseVisualStyleBackColor = true;
+            this.btnEditProducts.Click += new System.EventHandler(this.btnEditProducts_Click);
+            // 
+            // btnDeleteProducts
+            // 
+            this.btnDeleteProducts.Location = new System.Drawing.Point(274, 384);
+            this.btnDeleteProducts.Name = "btnDeleteProducts";
+            this.btnDeleteProducts.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteProducts.TabIndex = 15;
+            this.btnDeleteProducts.Text = "Delete";
+            this.btnDeleteProducts.UseVisualStyleBackColor = true;
+            this.btnDeleteProducts.Click += new System.EventHandler(this.btnDeleteProducts_Click);
+            // 
+            // InventoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 464);
+            this.Controls.Add(this.btnDeleteProducts);
+            this.Controls.Add(this.btnEditProducts);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnShowProducts);
+            this.Controls.Add(this.txtSportsCategory);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductId);
+            this.Controls.Add(this.lblSportsCategory);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.lblProductId);
+            this.Controls.Add(this.dgvInventory);
+            this.Controls.Add(this.lblBanner);
+            this.Name = "InventoryForm";
+            this.Text = "InventoryForm";
+            this.Load += new System.EventHandler(this.InventoryForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBanner;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Product_ID_Column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Product_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Stock_Column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price_Column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sports_Category;
+        private System.Windows.Forms.Label lblProductId;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblSportsCategory;
+        private System.Windows.Forms.TextBox txtProductId;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtSportsCategory;
+        private System.Windows.Forms.Button btnShowProducts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEditProducts;
+        private System.Windows.Forms.Button btnDeleteProducts;
+        public System.Windows.Forms.DataGridView dgvInventory;
+    }
+}
---
+++ InventoryForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FinalProjectVersion2
+{
+    public partial class InventoryForm : Form
+    {
+        int selectedRow, stock, oldStock, addOrSubtractStock, newStock;
+        double price;
+        string productId, productName, sportsCategory;
+        string filename = "SportsProducts.txt";
+        public InventoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEditProducts_Click(object sender, EventArgs e)
+        {
+            productId = txtProductId.Text;
+            productName = txtProductName.Text;
+            oldStock = Convert.ToInt32(dgvInventory[2, selectedRow].Value);
+            addOrSubtractStock = Convert.ToInt32(txtStock.Text);
+            price = Convert.ToDouble(txtPrice.Text);
+            sportsCategory = txtSportsCategory.Text;
+            newStock = oldStock + addOrSubtractStock;
+            dgvInventory[0, selectedRow].Value = productId;
+            dgvInventory[1, selectedRow].Value = productName;
+            dgvInventory[2, selectedRow].Value = newStock;
+            dgvInventory[3, selectedRow].Value = price;
+            dgvInventory[4, selectedRow].Value = sportsCategory;
+            ReturnAndClean();
+        }
+
+        private void btnDeleteProducts_Click(object sender, EventArgs e)
+        {
+            DialogResult deleteProduct;
+            deleteProduct = MessageBox.Show("Are you sure you want to delete this product?", "Delete Product", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (deleteProduct == DialogResult.Yes)
+            {
+                dgvInventory.Rows.RemoveAt(selectedRow);
+            }
+            SaveDGV();
+            CreateDGV();
+            ReturnAndClean();
+        }
+
+        private void dgvInventory_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = dgvInventory.CurrentRow.Index;
+            txtProductId.Text = Convert.ToString(dgvInventory[0, selectedRow].Value);
+            txtProductName.Text = Convert.ToString(dgvInventory[1, selectedRow].Value);
+            txtStock.Text = "0";
+            txtPrice.Text = Convert.ToString(dgvInventory[3, selectedRow].Value);
+            txtSportsCategory.Text = Convert.ToString(dgvInventory[4, selectedRow].Value);
+            lblStock.Text = "Add or Subtract Stock:";
+            btnAdd.Enabled = false;
+            btnEditProducts.Enabled = true;
+            btnDeleteProducts.Enabled = true;
+        }
+
+        private void InventoryForm_Load(object sender, EventArgs e)
+        {
+            CreateDGV();
+            ReturnAndClean();
+        }
+
+        public void CreateDGV()
+        {
+          dgvInventory.Rows.Clear();
+
+                using (var streamReader = new StreamReader(filename))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        var line = streamReader.ReadLine();
+                        var values = line.Split(',');
+                        var rowIndex = dgvInventory.Rows.Add();
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            dgvInventory.Rows[rowIndex].Cells[i].Value = values[i];
+                        }
+                    }
+                }
+        }
+
+        public void SaveDGV()
+        {
+            
+                const char DELIM = ',';
+
+                StreamWriter productFile;
+                productFile = File.CreateText(filename);
+                int rowcount = dgvInventory.Rows.Count;
+                for (int i = 0; i < rowcount; i++)
+                {
+                    productFile.WriteLine(dgvInventory.Rows[i].Cells[0].Value.ToString() + DELIM
+                        + dgvInventory.Rows[i].Cells[1].Value.ToString() + DELIM
+                        + dgvInventory.Rows[i].Cells[2].Value.ToString() + DELIM
+                        + dgvInventory.Rows[i].Cells[3].Value.ToString() + DELIM
+                        + dgvInventory.Rows[i].Cells[4].Value.ToString());
+                }
+                productFile.Close();
+        }
+
+        public void ReturnAndClean()
+        {
+            btnDeleteProducts.Enabled = false;
+            btnEditProducts.Enabled = false;
+            btnAdd.Enabled = true;
+            txtProductId.Text = "";
+            txtProductName.Text = "";
+            txtStock.Text = "";
+            txtPrice.Text = "";
+            txtSportsCategory.Text = "";
+            lblStock.Text = "Stock:";
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            const int MINIMUM_STOCK = 0;
+            const double MINIMUM_PRICE = 0.01;
+            try
+            {
+                productName = txtProductName.Text;
+                productId = txtProductId.Text;
+                sportsCategory = txtSportsCategory.Text;
+                stock = Convert.ToInt32(txtStock.Text);
+                price = Convert.ToDouble(txtPrice.Text);
+                if (stock < MINIMUM_STOCK)
+                {
+                    stock = MINIMUM_STOCK;
+                    MessageBox.Show("The minimum stock must be 0");
+                }
+
+                if (price < MINIMUM_PRICE)
+                {
+                    price = MINIMUM_PRICE;
+                    MessageBox.Show("The minimum price must be 0.01");
+                }
+                int n = dgvInventory.Rows.Add();
+                dgvInventory.Rows[n].Cells[0].Value = productId;
+                dgvInventory.Rows[n].Cells[1].Value = productName;
+                dgvInventory.Rows[n].Cells[2].Value = stock;
+                dgvInventory.Rows[n].Cells[3].Value = price;
+                dgvInventory.Rows[n].Cells[4].Value = sportsCategory;
+                SaveDGV();
+                CreateDGV();
+                ReturnAndClean();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
